cce8e6c5feeca2719419cc4b7b8d3d92ad5609b8
Jonathan Mataloni <jo.mataloni@gmail.com>
2024-01-24 22:35:29 +0100

    add features

--- server.py
+++ server.py
@@ -6,19 +6,21 @@ import os
 import time
 
 load_dotenv()
-global_client = None
+DELAY_SEC = os.getenv("DELAY_SEC", 5)
+SAT_NAME = os.getenv("SAT_NAME", "Adrastea")
+BROKER = os.getenv("BROKER", "localhost")
+DEVICE_ADDR = os.getenv("DEVICE_ADDR", "/dev/ttyUSB0")
+SYSTEM_NAME = os.getenv("SYSTEM_NAME", "Makai")
 
+global_client = None
+last_status = "ALIVE"
 
 driver = DalyBMS()
-driver.connect(device=os.getenv("DEVICE_ADDR"))
-
-# get_soc {'total_voltage': 13.2, 'current': 2.7, 'soc_percent': 75.9}
-# get_status {'cells': 4, 'temperature_sensors': 1, 'charger_running': False, 'load_running': False, 'states': {'DI1': False, 'DI2': True}, 'cycles': 127}
+driver.connect(device=DEVICE_ADDR)
 
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    # client.subscribe("Makai/battery/command")
+# def on_connect(client, userdata, flags, rc):
+# client.subscribe("Makai/battery/command")
 
 
 # def on_message(client, userdata, msg):
@@ -29,13 +31,18 @@ def mqtt_loop():
     global global_client
 
     client = mqtt.Client()
-    client.on_connect = on_connect
+    # client.on_connect = on_connect
     # client.on_message = on_message
 
     client.connect(os.getenv("BROKER"), 1883, 60)
 
     global_client = client
-    # client.loop_forever()
+
+
+def heartbeat_loop():
+    while True:
+        global_client.publish(f"{SYSTEM_NAME}/{SAT_NAME}/heartbeat", last_status)
+        time.sleep(1)
 
 
 mqtt_thread = threading.Thread(target=mqtt_loop)
@@ -44,11 +51,32 @@ mqtt_thread.start()
 while global_client is None:
     pass
 
-while True:
-    soc = driver.get_soc()
+heartbeat_loop = threading.Thread(target=heartbeat_loop)
+heartbeat_loop.start()
 
-    global_client.publish("Makai/battery/soc", soc["soc_percent"])
-    global_client.publish("Makai/battery/current", soc["current"])
-    global_client.publish("Makai/battery/voltage", soc["total_voltage"])
+while True:
+    try:
+        soc = driver.get_soc()
+        temps = driver.get_temperatures()
+        status = driver.get_mosfet_status()
+
+        global_client.publish(f"{SYSTEM_NAME}/battery/soc", soc["soc_percent"])
+        global_client.publish(f"{SYSTEM_NAME}/battery/current", soc["current"])
+        global_client.publish(f"{SYSTEM_NAME}/battery/voltage", soc["total_voltage"])
+        global_client.publish(f"{SYSTEM_NAME}/battery/temperature", temps[1])
+        global_client.publish(f"{SYSTEM_NAME}/battery/mode", status["mode"])
+        global_client.publish(f"{SYSTEM_NAME}/battery/capacity", status["capacity_ah"])
+        global_client.publish(
+            f"{SYSTEM_NAME}/battery/charging_locked", not status["charging_mosfet"]
+        )
+        global_client.publish(
+            f"{SYSTEM_NAME}/battery/discharging_locked",
+            not status["discharging_mosfet"],
+        )
+
+        last_status = "ALIVE"
+    except Exception as e:
+        last_status = "ERROR: " + str(e)
+        print(e)
 
     time.sleep(5)
